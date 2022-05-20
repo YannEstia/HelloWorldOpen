5b9ec5e52ae02a3299df253e17ecc00d7cb1a42d
Yann-Alan PILATTE <yann-alan.pilatte@estia.local>
2022-05-20 14:31:08 +0200

    Added 3rd arg to addition() but it's d

--- operationDeCalcul.py
+++ operationDeCalcul.py
@@ -1,15 +1,10 @@
-def addition(a,b):
-    return(a+b)
+def addition(a,b,d):
+    return(a+b+d)
 
 def soustraction(a,b):
     return(a-b)
 
-<<<<<<< HEAD
 def multiplication(a,b,d,e):
     return(a*b*d*e)
 
 #commentaire
-=======
-def multiplication(a,b,c,d):
-    return(a*b*c*d)
->>>>>>> test_branch
